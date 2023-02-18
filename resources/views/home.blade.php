bfa908ae2fd0ca0df650cadc6a2a5913e04adbbd
manolioluigi <luigi.manolio1999@gmail.com>
2023-02-18 17:20:31 +0100

    main homepage + inizio struttura url

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Comics</title>
-    @vite('resources/js/app.js')
-</head>
-<body>
-<!--Header-->
-    @include('partials.header')
-    
-<!--Main-->
+@extends('layouts.layout')
+@section('content')
 
-    MAIN
+    <main>
 
-<!--Footer-->
-    @include('partials.footer')
+        <div class="container-fluid mt-4">
+            <!--prima row-->
+            <div class="row">
+                <div class="col-12 no-padding relative">
+                    <img src="{{ Vite::asset('resources\images\jumbotron.jpg') }}" alt="jumbotron" class="jumbo">
+                    <div class="blue-tag d-flex flex-row align-items-center">
+                        <h5 class="white no-margin">CURRENT SERIES</h5>
+                    </div>
+                </div>
+            </div>
+            <!--seconda row-->
+            <div class="col-12">
+                <div class="row bg-main py-5">
+                    <div class="col-12 d-flex flex-column align-items-center">
+                        
+                        <div class="card-container">
 
-</body>
-</html>
+                            @foreach ($comics as $key => $item)
+                                
+                                <a href="#" class="comic-card">
+                                    <img src="{{ $item['thumb'] }}" alt="{{ $item['title'] }}">
+                                    <span class="white mt-2">{{ $item['title'] }}</span>
+                                </a>
+
+                            @endforeach
+
+                        </div>
+
+                        <div class="blue-tag-small mt-5">
+                            <span class="white">LOAD MORE</span>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+@endsection
